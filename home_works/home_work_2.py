08a457a4abd5bba193fd8e6585b31420dcbe80cc
Roman Vrublevskiy <roman.vrublevskiy@gmail.com>
2016-12-09 15:52:02 +0000

    Add possibility to go back from delete function

--- home_works/home_work_2.py
+++ home_works/home_work_2.py
@@ -13,8 +13,8 @@
 
 import sys, os, datetime
 
-# 1
 
+# 5.
 def log():
     curtime = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
     logfile = open('log.txt', 'a')
@@ -32,12 +32,19 @@ def print_students():
     for num, (name, last_name, age) in enumerate(students):
         print("{num}. {last_name} {name}".format(name=name, last_name = last_name, num = num))
 
+# 6.
 def del_student():
     global students
-    index = int(input("\nEnter number of student you want to delete\n"))
+    index = input("\nEnter number of student you want to delete\n.To return to main menu press 'b")
+    if index.lower() == "b":
+        return 0
+    else:
+        index = int(index)
+
     removed = students.pop(index)
     print("Student {} {} was removed.\n".format(removed[1], removed[0]))
 
+# 1
 def menu_function(option=None):
     if not option:
         option = input(menu)
@@ -46,6 +53,7 @@ def menu_function(option=None):
         print("Quiting")
         wfile = open(filename, 'w')
         for (name, last_name, age) in students:
+
             # 3
             wfile.write("{last_name} {name} {age}\n".format(name = name, last_name = last_name, age = age))
         sys.exit(0)
@@ -72,12 +80,10 @@ students = []
 filename = 'students.txt'
 
 # 4.
-
 if os.path.isfile(filename):
     for line in open(filename, 'r'):
         students.append(line.strip().split())
 
 # 2
-
 while True:
     menu_function()
