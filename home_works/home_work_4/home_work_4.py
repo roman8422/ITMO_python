87efdf9d5dd2228309fe8669a13aaff5be22cf97
Roman Vrublevskiy <roman.vrublevskiy@gmail.com>
2016-12-17 14:27:39 +0000

    Add grep function

--- home_works/home_work_4/home_work_4.py
+++ home_works/home_work_4/home_work_4.py
@@ -27,3 +27,31 @@ dir = 'python3'
 print("Dir {dir} size is - {size}".format(dir=dir, size=du(dir, recursive=False)))
 print()
 print("Dir {dir} size is - {size}".format(dir=dir, size=du(dir)))
+
+def grep(dir, str, recursive=False, ignorecase=False):
+    paths_list = os.listdir(dir)
+    for subpath in paths_list:
+        if ignorecase:
+            name_to_check = subpath.lower()
+            str = str.lower()
+        else:
+            name_to_check = subpath
+
+        full_path = os.path.join(dir, subpath)
+        if str in name_to_check:
+            print("Filename matches template:", full_path)
+        if os.path.isdir(full_path):
+            if recursive:
+                grep(full_path, str, recursive, ignorecase)
+
+    return 0
+
+dir = 'python3'
+
+str = 'Pyt'
+print('\nstr =', str)
+print(grep(dir, str, ignorecase=True))
+
+str = 'yth'
+print('\nstr =', str)
+print(grep(dir, str, ignorecase=True, recursive=True))
