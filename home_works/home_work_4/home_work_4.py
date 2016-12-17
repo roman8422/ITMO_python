7f58b85a7c1b727c95349ee9b82d062edf4cb6cb
Roman Vrublevskiy <roman.vrublevskiy@gmail.com>
2016-12-17 11:44:19 +0000

    Implement du function for files

--- home_works/home_work_4/home_work_4.py
+++ home_works/home_work_4/home_work_4.py
@@ -6,3 +6,24 @@
 # -  функция2 - пробегает по указанной директории,
 #    находит все файлы с указанной подстрокой в имени
 # * в самом файле
+
+import os
+
+def du(path):
+    size = 0
+    paths_list = os.listdir(path)
+    for subpath in paths_list:
+        path_to_check = os.path.join(path, subpath)
+        if os.path.isfile(path_to_check):
+            size += os.path.getsize(path_to_check)
+            print(os.path.getsize(path_to_check), path_to_check)
+    return size
+
+print(du("python3"))
+
+# Verification:
+# find python3/ -maxdepth 1 -type f  | xargs du -sb | awk 'BEGIN{sum=0};{sum+=$1;print $0};END{print sum}'
+# 2158    python3/python.mk
+# 324     python3/debian_defaults
+# 11203   python3/py3versions.py
+# 13685
