926f60c80566159328e106b1276426f0d318fdbc
Roman Vrublevskiy <roman.vrublevskiy@gmail.com>
2016-12-29 18:20:56 +0000

    Add uncovered field drawing at the end

--- test2/task2_saper.py
+++ test2/task2_saper.py
@@ -62,13 +62,13 @@ class Saper:
 
                     self.field[x][y][0] = num_of_mines_around
 
-    def draw_field(self):
+    def draw_field(self, open_field = False):
         print()
         count = 0
         for x in range(self.field_size):
             line = ""
             for y in range(self.field_size):
-                line += str(self.field[x][y][0]) if self.field[x][y][1] == 'open' else 'X'
+                line += str(self.field[x][y][0]) if open_field or self.field[x][y][1] == 'open' else 'X'
                 line += " "
 
             print(line)
@@ -114,17 +114,18 @@ class Saper:
 
 
     def start_game(self):
-        self.draw_field()
         while True:
-            self.open_cell()
             self.draw_field()
+            self.open_cell()
             if not self.exploded:
                 self.check_field()
 
                 if not self.cells_left:
+                    self.draw_field(open_field=True)
                     print("You've won!!!\nCongratz!")
                     break
             else:
+                self.draw_field(open_field=True)
                 print("You've exploded\n")
                 break
 
