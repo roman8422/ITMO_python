98fb59dd1a05f6f661f8f8b669d44a19ce644cb0
Roman Vrublevskiy <roman.vrublevskiy@gmail.com>
2017-01-19 22:22:41 +0300

    Disable controls if not loggedin

--- home_works/pyqt/loginwindow/gui/MainWindow.py
+++ home_works/pyqt/loginwindow/gui/MainWindow.py
@@ -11,19 +11,34 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self, *args, **kwargs):
         super(MainWindow, self).__init__(*args, **kwargs)
 
+        self.loggedin = False
+
         self.init_ui()
         self.init_signals()
 
-        self.loggedin = False
 
     def init_ui(self):
         self.setupUi(self)
 
         self.notesWidget = NotesWidget(self)
         self.stackedWidget.addWidget(self.notesWidget)
+
+        if not self.loggedin:
+            self.disable_controls()
+        else:
+            self.enable_controls()
+
         self.loginWidget = LoginWidget(self)
         self.stackedWidget.addWidget(self.loginWidget)
         self.stackedWidget.setCurrentWidget(self.loginWidget)
 
     def init_signals(self):
         pass
+
+    def disable_controls(self):
+        self.toolBar.setEnabled(False)
+        self.menubar.setEnabled(False)
+
+    def enable_controls(self):
+        self.toolBar.setEnabled(True)
+        self.menubar.setEnabled(True)
