5382e1593e4365820a7566d451435cc563332498
Roman Vrublevskiy <roman.vrublevskiy@gmail.com>
2017-01-19 22:44:45 +0300

    Add username processing

--- home_works/pyqt/loginwindow/gui/LoginWidget.py
+++ home_works/pyqt/loginwindow/gui/LoginWidget.py
@@ -8,13 +8,24 @@ class LoginWidget(QWidget, Ui_loginDialog):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
 
+        self.creds = {
+            'user': {
+                'password': '123'
+            }
+        }
+
         self.init_ui()
 
     def init_ui(self):
         self.setupUi(self)
 
     def accept(self):
-        pass
+        username_entered = self.loginField.text()
+        if username_entered in self.creds:
+            print(self.creds[username_entered])
+        else:
+            self.loginField.setText('')
+            self.pwField.setText('')
 
     def reject(self):
         pass
